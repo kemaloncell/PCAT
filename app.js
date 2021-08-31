dfdd499b374c297e588ac0d11136ce7805993f28
kemaloncell <57701397+kemaloncell@users.noreply.github.com>
2021-08-31 16:39:17 +0300

    nodemaon and express package are  dowlanded and server created

--- app.js
+++ app.js
@@ -1,3 +1,15 @@
-const fn = function () {
-  console.log('funchtion  working');
-};
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  const photo = {
+    id: 1,
+    name: 'photo name',
+    description: 'photo description',
+  };
+  res.send(photo);
+});
+const port = 3000;
+app.listen(3000, () => {
+  console.log(`Server port ${port} started`);
+});

67638ebefbd026c394990f25dbc1a61bacaf1a7c
kemaloncell <57701397+kemaloncell@users.noreply.github.com>
2021-09-01 15:39:32 +0300

    fixed folder structure with template engine

--- app.js
+++ app.js
@@ -1,12 +1,26 @@
 const express = require('express');
-const app = express();
+const ejs = require('ejs');
 const path = require('path');
-// MIDDLEWARES
+const app = express();
+
+//* Template engine
+app.set('view engine', 'ejs');
+
+//* Middlewares
 app.use(express.static('public'));
 
+//* Routes
 app.get('/', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'temp/index.html'));
+  //res.sendFile(path.resolve(__dirname, 'temp/index.html')); zor yol :)
+  res.render('index');
 });
+app.get('/about', (req, res) => {
+  res.render('about');
+});
+app.get('/add', (req, res) => {
+  res.render('add');
+});
+
 const port = 3000;
 app.listen(port, () => {
   console.log(`Server port ${port} started`);

6bbc5e3bcc52e8d89a28ee17b3b11952de9fb677
kemaloncell <57701397+kemaloncell@users.noreply.github.com>
2021-09-02 00:17:30 +0300

    Model file created and data sent to index page

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
 const express = require('express');
+const Photo = require('./models/Photo');
 const ejs = require('ejs');
 const path = require('path');
+const mongoose = require('mongoose');
 const app = express();
+
+// Connect DB
+mongoose.connect('mongodb://localhost/pcat-test-db', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 //* Template engine
 app.set('view engine', 'ejs');
@@ -11,9 +19,12 @@ app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 //* Routes
-app.get('/', (req, res) => {
+app.get('/', async (req, res) => {
   //res.sendFile(path.resolve(__dirname, 'temp/index.html')); zor yol :)
-  res.render('index');
+  const photos = await Photo.find({});
+  res.render('index', {
+    photos,
+  });
 });
 app.get('/about', (req, res) => {
   res.render('about');
@@ -21,8 +32,8 @@ app.get('/about', (req, res) => {
 app.get('/add', (req, res) => {
   res.render('add');
 });
-app.post('/photos', (req, res) => {
-  console.log(req.body);
+app.post('/photos', async (req, res) => {
+  await Photo.create(req.body); //will wait until the document is created in the database
   res.redirect('/');
 });
 

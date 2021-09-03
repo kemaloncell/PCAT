83452f89516c565b2ee200ea3d220749fc5a5f6d
kemaloncell <57701397+kemaloncell@users.noreply.github.com>
2021-09-03 19:51:18 +0300

    file delteted done

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const Photo = require('./models/Photo');
 const fileUpload = require('express-fileupload');
+const methodOverride = require('method-override');
 const ejs = require('ejs');
 const path = require('path');
 const mongoose = require('mongoose');
@@ -21,6 +22,11 @@ app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(fileUpload());
+app.use(
+  methodOverride('_method', {
+    methods: ['POST', 'GET'],
+  })
+);
 
 //* Routes
 app.get('/', async (req, res) => {
@@ -45,6 +51,12 @@ app.get('/add', (req, res) => {
 app.get('/photo', (req, res) => {
   res.render('photo');
 });
+app.get('/photos/edit/:id', async (req, res) => {
+  const photo = await Photo.findOne({ _id: req.params.id });
+  res.render('edit', {
+    photo,
+  });
+});
 app.post('/photos', async (req, res) => {
   const uploadDir = 'public/uploads';
   if (!fs.existsSync(uploadDir)) {
@@ -61,7 +73,21 @@ app.post('/photos', async (req, res) => {
     res.redirect('/');
   });
 });
+app.put('/photos/:id', async (req, res) => {
+  const photo = await Photo.findOne({ _id: req.params.id });
+  photo.title = req.body.title;
+  photo.description = req.body.description;
+  photo.save();
+  res.redirect(`/photos/${req.params.id}`);
+});
 
+app.delete('/photos/:id', async (req, res) => {
+  const photo = await Photo.findOne({ _id: req.params.id });
+  let deletedImage = __dirname + '/public' + photo.image;
+  fs.unlinkSync(deletedImage);
+  await Photo.findByIdAndRemove(req.params.id);
+  res.redirect('/');
+});
 const port = 3000;
 app.listen(port, () => {
   console.log(`Server port ${port} started`);

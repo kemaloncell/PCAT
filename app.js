fc1c2e0c8d16e09bc498a565ae46cc16b74953a8
kemaloncell <57701397+kemaloncell@users.noreply.github.com>
2021-09-07 14:10:41 +0300

    fixed the connexted sentences

--- app.js
+++ app.js
@@ -5,16 +5,15 @@ const ejs = require('ejs');
 const mongoose = require('mongoose');
 const photoController = require('./controllers/PhotoControllers');
 const pageController = require('./controllers/PageController');
+const Photo = require('./models/Photo');
 
 const app = express();
 
 //* Connect DB
 mongoose
-  .connect('mongodb+srv://kemal:614175123@cluster0.ugvlx.mongodb.net/pcat-db?retryWrites=true&w=majority', {
-    // useNewUrlParser: true,
-    // useUnifiedTopology: true,
-    // useCreateIndex: true,
-    // usefindAndModify: false,
+  .connect('mongodb://kemal:nkfh3Rsx6iPBa7sB@cluster0-shard-00-00.nbotz.mongodb.net:27017,cluster0-shard-00-01.nbotz.mongodb.net:27017,cluster0-shard-00-02.nbotz.mongodb.net:27017/pcat-db?ssl=true&replicaSet=atlas-clc38i-shard-0&authSource=admin&retryWrites=true&w=majority', {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
   })
   .then(() => {
     console.log('DB connected !');
